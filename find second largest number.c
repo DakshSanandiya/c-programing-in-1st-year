1ce38c983c443429e9eb2fd2b60565b377fafdb0
DakshSanandiya <143684719+DakshSanandiya@users.noreply.github.com>
2023-09-05 22:40:47 +0530

    Add files via upload

---
+++ find second largest number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main (){
+	
+	int a,b,c;
+	printf("enter any 3 int number: ");
+	scanf("%d %d %d",&a,&b,&c);
+	
+	if(a>b && a>c && c>b){
+		printf("%d is second largest",b);
+	}else if(a>b && a>c && b>c){
+		printf("%d is second largest",c);
+	}else{
+		printf("%d is second largest",a);
+	}
+}
