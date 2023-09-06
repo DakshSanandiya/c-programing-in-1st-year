1c4a40c7015f2c1145b0e12f151b16d8e75eda3c
DakshSanandiya <143684719+DakshSanandiya@users.noreply.github.com>
2023-09-06 10:47:35 +0530

    Update find second largest number.c

--- find second largest number.c
+++ find second largest number.c
@@ -5,12 +5,25 @@ void main (){
 	int a,b,c;
 	printf("enter any 3 int number: ");
 	scanf("%d %d %d",&a,&b,&c);
-	
-	if(a>b && a>c && c>b){
-		printf("%d is second largest",c);
-	}else if(a>b && a>c && b>c){
-		printf("%d is second largest",b);
-	}else{
-		printf("%d is second largest",a);
-	}
+	if(a>b){
+		if(a>c){
+			if(c>b){
+				printf("%d is second largest",c);
+			}else {
+				printf("%d is second largest",b);
+			}
+		}else {
+			printf("%d is second largest",a);
+		}
+	}else {
+	if(b>c){
+			if(a>c){
+				printf("%d is second largest",a);
+			}else {
+				printf("%d is second largest",c);
+			}
+		}else {
+			printf("%d is second largest",b);
+		}
+		}
 }
