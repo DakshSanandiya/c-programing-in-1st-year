2cf4f59e59fe764919fea38766858c8b9de138d5
DakshSanandiya <143684719+DakshSanandiya@users.noreply.github.com>
2023-09-11 09:21:07 +0530

    Add files via upload

---
+++ Perform Addition, Subtraction, Multiplication and Division of 2 numbers as per user’s choice using switch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>.
+
+void main () {
+	int a,b,c;
+	char z;
+	printf("enter your any 2 int number: ");
+	scanf("%d %d",&a,&b);
+	
+	printf("enter any  operator(+,-,*,/): ");
+	scanf(" %c",&z);
+	
+	switch(z){
+		case '+':
+			c = a + b;
+		break;
+		case '-':
+			c = a - b;
+		break;
+		case '*':
+			c = a * b;
+		break;
+		case '/':
+			c = a / b;
+		break;
+	}
+	printf("%d",c);
+}
