8fea480ccbb3bced74b8df7e5063bfa76a6f776e
DakshSanandiya <143684719+DakshSanandiya@users.noreply.github.com>
2023-09-25 22:57:45 +0530

    Add files via upload

---
+++ 21-09-23/Find factors of the given number.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+{
+	int n,i=1;
+	printf("enter any int number: ");
+	scanf("%d",&n);
+	
+	while (i<=n){
+		if (n % i == 0){
+			printf("%d\n",i);
+		
+		}i++;	
+	}
+	
+}
